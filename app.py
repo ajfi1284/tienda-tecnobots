fb4619e59435e37d8c004b01305d831628f5d702
ajfi1284 <ajfi1284@gmail.com>
2026-04-17 13:59:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -336,6 +336,12 @@ def login():
     
     return render_template('login.html')
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 @app.route('/perfil', methods=['GET', 'POST'])
 @login_required
 def perfil():
